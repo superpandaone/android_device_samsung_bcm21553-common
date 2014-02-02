4c56e42e7f79a13bf1836dea50cfabe9d3a76dae
Biel Tura <bieltv.3@gmail.com>
2014-02-02 09:10:51 +0100

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -23,67 +23,70 @@
 LOCAL_PATH:= $(call my-dir)
 
 # Board
-TARGET_ARCH_VARIANT := armv6-vfp
-TARGET_CPU_ABI := armeabi-v6l
-TARGET_CPU_ABI2 := armeabi
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
+TARGET_ARCH_VARIANT :=				armv6-vfp
+TARGET_CPU_ABI := 				armeabi-v6l
+TARGET_CPU_ABI2 := 				armeabi
+TARGET_NO_BOOTLOADER := 			true
+TARGET_NO_RADIOIMAGE := 			true
 
 # Audio
-BOARD_USES_GENERIC_AUDIO := false
-BOARD_USES_ALSA_AUDIO := true
-BOARD_PREBUILT_LIBAUDIO := true
-BUILD_WITH_ALSA_UTILS := true
+BOARD_USES_GENERIC_AUDIO := 			false
+BOARD_USES_ALSA_AUDIO := 			true
+BOARD_PREBUILT_LIBAUDIO := 			true
+BUILD_WITH_ALSA_UTILS := 			true
 
 # Camera
-USE_CAMERA_STUB := false
+USE_CAMERA_STUB := 				false
 #ifeq ($USE_CAMERA_STUB),false)
-#BOARD_CAMERA_LIBRARIES := libcamera
-#BOARD_USE_CAF_LIBCAMERA_GB_REL := true
-#BOARD_CAMERA_USE_GETBUFFERINFO := true
+#BOARD_CAMERA_LIBRARIES := 			libcamera
+#BOARD_USE_CAF_LIBCAMERA_GB_REL := 		true
+#BOARD_CAMERA_USE_GETBUFFERINFO := 		true
 #endif
 
 # 3D
-BOARD_EGL_CFG := device/samsung/bcm21553-common/prebuilt/lib/egl/egl.cfg
-BOARD_USES_HGL := true
-TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
-USE_OPENGL_RENDER := true
-TARGET_ELECTRONBEAM_FRAMES := 20
+BOARD_EGL_CFG := 				device/samsung/bcm21553-common/prebuilt/lib/egl/egl.cfg
+BOARD_USES_HGL :=				true
+BOARD_AVOID_DRAW_TEXTURE_EXTENSION :=		true
+TARGET_LIBAGL_USE_GRALLOC_COPYBITS :=		true
+TARGET_USES_16BPPSURFACE_FOR_OPAQUE:=		true
+TARGET_LIBAGL_USE_GRALLOC_COPYBITS := 		true
+USE_OPENGL_RENDER := 				true
+TARGET_ELECTRONBEAM_FRAMES := 			20
 
 # Mobile Data
-BOARD_MOBILEDATA_INTERFACE_NAME := "pdp0"
+BOARD_MOBILEDATA_INTERFACE_NAME := 		"pdp0"
 
 # Device related defines
-BOARD_NAND_PAGE_SIZE := 4096 -s 128
-BOARD_KERNEL_BASE := 0x81600000
-BOARD_KERNEL_PAGESIZE := 4096
-BOARD_PAGE_SIZE := 0x00001000
+BOARD_NAND_PAGE_SIZE := 			4096 -s 128
+BOARD_KERNEL_BASE := 				0x81600000
+BOARD_KERNEL_PAGESIZE := 			4096
+BOARD_PAGE_SIZE := 				0x00001000
 
 # JIT / Optimizations
-WITH_JIT := true
-ENABLE_JSC_JIT := true
-BOARD_USE_SCREENCAP := true
-JS_ENGINE := v8
+WITH_JIT := 					true
+ENABLE_JSC_JIT := 				true
+BOARD_USE_SCREENCAP := 				true
+JS_ENGINE := 					v8
 
 # USB
-BOARD_USE_USB_MASS_STORAGE_SWITCH := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/lm-2/gadget/lun0/file"
-BOARD_UMS_LUNFILE := "/sys/devices/lm-2/gadget/lun0/file"
+BOARD_USE_USB_MASS_STORAGE_SWITCH := 		true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := 		"/sys/devices/lm-2/gadget/lun0/file"
+BOARD_UMS_LUNFILE := 				"/sys/devices/lm-2/gadget/lun0/file"
 
 # Wifi
-BOARD_WPA_SUPPLICANT_DRIVER := WEXT
-WPA_SUPPLICANT_VERSION := VER_0_6_X
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/dhd.ko"
-WIFI_DRIVER_FW_STA_PATH := "/system/etc/wifi/bcm4330_sta.bin"
-WIFI_DRIVER_FW_AP_PATH := "/system/etc/wifi/bcm4330_aps.bin"
-WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wifi/bcm4330_sta.bin nvram_path=/system/etc/wifi/nvram.txt"
-WIFI_DRIVER_MODULE_NAME := "bcm4330"
+BOARD_WPA_SUPPLICANT_DRIVER := 			WEXT
+WPA_SUPPLICANT_VERSION := 			VER_0_6_X
+WIFI_DRIVER_MODULE_PATH := 			"/system/lib/modules/dhd.ko"
+WIFI_DRIVER_FW_STA_PATH := 			"/system/etc/wifi/bcm4330_sta.bin"
+WIFI_DRIVER_FW_AP_PATH := 			"/system/etc/wifi/bcm4330_aps.bin"
+WIFI_DRIVER_MODULE_ARG := 			"firmware_path=/system/etc/wifi/bcm4330_sta.bin nvram_path=/system/etc/wifi/nvram.txt"
+WIFI_DRIVER_MODULE_NAME := 			"bcm4330"
 
 # Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_HAVE_BLUETOOTH := 			true
+BOARD_HAVE_BLUETOOTH_BCM := 			true
 
 # FM
-BOARD_HAVE_FM_RADIO := true
-BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
-BOARD_FM_DEVICE := bcm4329
+BOARD_HAVE_FM_RADIO := 				true
+BOARD_GLOBAL_CFLAGS += 				-DHAVE_FM_RADIO
+BOARD_FM_DEVICE := 				bcm4329

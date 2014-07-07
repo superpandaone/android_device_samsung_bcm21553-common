bf857936f5e3e513eb97a4b37a5908c9e49016aa
Alberto96 <albertoguanti@gmail.com>
2014-07-07 22:27:50 +0200

    common: temp. disable VideoEditor until Hantro Codecs gets fixed

--- common.mk
+++ common.mk
@@ -249,5 +249,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # We have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 
+# Packages
+BOARD_NO_HWCODECS := true
+
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
